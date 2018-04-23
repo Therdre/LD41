da830fce99dbf4b2d7bf36d506db307d5960b116
Maybri Flores <maybri@gmail.com>
2018-04-23 13:48:32 -0700

    Menu

---
+++ Assets/UI/Scripts/MainMenu.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : Singleton<MainMenu>
+{
+    public GameObject menuRoot = null;
+    private void Start()
+    {
+        ShowMenu();
+    }
+
+    public void ShowMenu()
+    {
+        menuRoot.SetActive(true);
+        Time.timeScale = 0.0f;
+    }
+
+    public void StartGame()
+    {
+        menuRoot.SetActive(false);
+        Time.timeScale = 1.0f;
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
