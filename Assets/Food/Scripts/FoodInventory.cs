dca2130deac4895482dd3f3d39f4918452c6aaf4
gwanogdab <luisarmstrong3d@hotmail.com>
2018-04-23 05:42:58 -0700

    Revert "Items to display in the inventory"
    This reverts commit d6ef0856d31e6b618b04b48c3146e2d2cc200d3e.

--- Assets/Food/Scripts/FoodInventory.cs
+++ Assets/Food/Scripts/FoodInventory.cs
@@ -9,21 +9,13 @@ namespace FoodNameSpace
     public class FoodInventory : Singleton<FoodInventory>
     {
         public List<Food> rawFood = new List<Food>();
-        public List<FoodIcons> inventoryDisplay = new List<FoodIcons>();
 
         List<ExistingFood> foodInventory = new List<ExistingFood>();
-        int maxInventoryNumber = 0;
-        int inventorySize = 0;
+
         void Start()
         {
             foodInventory.Clear();
             PopulateRawFood();
-            maxInventoryNumber = inventoryDisplay.Count;
-
-            for(int i=0;i<inventoryDisplay.Count;++i)
-            {
-                inventoryDisplay[i].gameObject.SetActive(false);
-            }
         }
         void PopulateRawFood()
         {
@@ -36,10 +28,6 @@ namespace FoodNameSpace
 
         public void AddFood(Tag tag, ExistingFood food)
         {
-            if(inventorySize==maxInventoryNumber)
-            {
-                return;
-            }
             //check if the food with the tag already exists, if it does
             //increment it
             List<ExistingFood> foodType = foodInventory.FindAll(x => x.IsFoodType(food));
@@ -49,17 +37,19 @@ namespace FoodNameSpace
             if(foodWithTag!=null)
             {
                 foodWithTag.IncreaseQuantity();
-                DisplayFood(foodWithTag);
             }
             else
             {
                 ExistingFood newFood = new ExistingFood(food);
                 newFood.AddTag(tag);
                 foodInventory.Add(newFood);
-                DisplayFood(newFood);
             }
-            inventorySize++;
-            RemoveFood(food);
+            food.DecreaseQuantity();
+
+            if(food.GetQuantity()<=0)
+            {
+                foodInventory.Remove(food);
+            }
         }
 
         public void RemoveRecipeIngredients(Recipe recipe)
@@ -78,25 +68,16 @@ namespace FoodNameSpace
             if (food == null)
                 return;
 
-            if (food.GetTags().Count > 0)
-            {
-                inventorySize--;
-            }
             food.DecreaseQuantity();
 
             if (food.GetQuantity() <= 0)
             {
                 foodInventory.Remove(food);
             }
-            
         }
 
         public List<ExistingFood> GetAvailableFood(Tag tag)
         {
-            if (inventorySize == maxInventoryNumber)
-            {
-                return new List<ExistingFood>();
-            }
             List<ExistingFood> availableFood = foodInventory.FindAll(x => !x.TagTypeExists(tag.tagType));
             
             return availableFood;
@@ -105,18 +86,6 @@ namespace FoodNameSpace
         public List<ExistingFood> GetAllInvetoryFood()
         {
             return foodInventory;
-        }
-
-        public void DisplayFood(ExistingFood food)
-        {
-            //get available inventory space
-            FoodIcons availableSpace = inventoryDisplay.Find(x=>!x.gameObject.activeSelf);
-            if (availableSpace == null)
-                return;
-
-            availableSpace.SetGraphics(food);
-            availableSpace.gameObject.SetActive(true);
-
         }
     }
 }
